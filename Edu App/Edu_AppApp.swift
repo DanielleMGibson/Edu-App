6b8250effef05ac17f9add4294344df46eaef09a
Danielle Gibson <peyton.gibson@stu.barren.kyschools.us>
2024-11-26 09:31:51 -0600

    Initial Commit

---
+++ Edu App/Edu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edu_AppApp.swift
+//  Edu App
+//
+//  Created by Student on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Edu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
